8412d5b13e350c0749aefe91ada9551a3c3a0c01
Vitor Lopes <137239415+vitor1209@users.noreply.github.com>
2024-09-10 23:08:46 -0300

    Add files via upload

--- jogoImpostor/main.py
+++ jogoImpostor/main.py
@@ -7,14 +7,14 @@ from Videogames import perguntasVideogames
 import random
 import os
 
-class JogoComidas():
-    def __init__(self, nomes):
+class Jogo:
+    def __init__(self, nomes, categoria, perguntas):
         self.jogadores = {}
         self.j = 1
-        self.comidas = comidas
+        self.categoria = categoria
         self.nomes = nomes
-        self.comida = random.choice(self.comidas)
-        self.perguntas = perguntasComidas
+        self.palavra = random.choice(self.categoria)
+        self.perguntas = perguntas
         self.impostor = random.choice(self.nomes)
 
     def part(self):
@@ -38,7 +38,7 @@ class JogoComidas():
                 pesCerta = str(input(f"Você é {pessoa}? "))
                 print()
                 if pesCerta == "SIM" or "Sim" or "sim":
-                    print(f" A comida é: {self.comida} ".center(100, '-').upper())
+                    print(f" A palavra é: {self.palavra} ".center(100, '-').upper())
                     input("Aperte Qualquer tecla para continuar ")
                     os.system('cls')
 
@@ -85,10 +85,10 @@ class JogoComidas():
 
     def finalizar(self):
         ListaComidas = []
-        ListaComidas.append(self.comida)
+        ListaComidas.append(self.palavra)
         i = 0
         while i != 3:
-            ListaComidas.append(random.choice(self.comidas))
+            ListaComidas.append(random.choice(self.categoria))
             i += 1
         random.shuffle(ListaComidas)
         maiorVar = max(self.jogadores.values())
@@ -111,249 +111,17 @@ class JogoComidas():
         print()
         print(f"O Impostor era {self.impostor}".center(100, '-').upper())
         print()
-        print(f"{self.impostor} qual comida era? ".center(100, '-').upper())
+        print(f"{self.impostor} qual palavra era? ".center(100, '-').upper())
         print()
         print(f'Pode ser: {", ".join(ListaComidas)}'.upper())
         print()
         resposta = input("Qual é: ").upper()
-        if resposta == self.comida.upper():
+        if resposta == self.palavra.upper():
             print()
             print("acertou".upper())
         else :
             print()
-            print("não era essa a comida".upper())
-
-class JogoCelebridades():
-    def __init__(self, nomes):
-        self.jogadores = {}
-        self.j = 1
-        self.celebridades = celebridades
-        self.nomes = nomes
-        self.celebridade = random.choice(self.celebridades)
-        self.perguntas = perguntasCelebridades
-        self.impostor = random.choice(self.nomes)
-
-    def part(self):
-        for self.nome in self.nomes:
-            self.jogadores[self.nome] = 0
-
-    def DividirImpostor(self):
-        for pessoa in self.nomes:
-            if pessoa == self.impostor:
-                print()
-                print("Escreva SIM para confirmar")
-                pesCerta = str(input(f"Você é {pessoa}? "))
-                print()
-                if pesCerta == "SIM" or "Sim" or "sim":
-                    print(f" Você é o impostor".center(100, '-').upper())
-                    input("Aperte Qualquer tecla para continuar ")
-                    os.system('cls')
-            else :
-                print()
-                print("Escreva SIM para confirmar")
-                pesCerta = str(input(f"Você é {pessoa}? "))
-                print()
-                if pesCerta == "SIM" or "Sim" or "sim":
-                    print(f" A celebridade é: {self.celebridade} ".center(100, '-').upper())
-                    input("Aperte Qualquer tecla para continuar ")
-                    os.system('cls')
-
-    def perguntar(self):
-        for self.nome in self.nomes:
-            print()
-            print(f"{self.nome} Pergunta para: {self.nomes[self.j]}".center(100, '-').upper())
-            print()
-            print(random.choice(self.perguntas))
-            print()
-            input("Aperte Qualquer tecla para continuar ")
-            os.system('cls')
-            self.j += 1
-            if self.j == len(self.nomes):
-                self.j = 0
-    
-    def votar(self):
-     for pessoa in self.nomes:
-            os.system('cls')
-            print("Votação".center(100, '-').upper())
-            print()
-            print("Escreva SIM para confirmar")
-            pesCerta = str(input(f"Você é {pessoa}? "))
-            print()
-            if pesCerta == "SIM" or "Sim" or "sim":
-               posibilitVoto = []
-               for i in self.nomes:
-                   if i != pessoa:
-                    posibilitVoto.append(i)
-            while True:
-                    print(f"Você pode votar em: {', '.join(posibilitVoto)}")
-                    voto = input(f"Votar em: ").upper()
-                    if voto == pessoa:
-                        print()
-                        print("Você não pode votar em si mesmo")
-                        print()
-                    elif voto in posibilitVoto:
-                        self.jogadores[voto] += 1
-                        break  
-                    else:
-                        print()
-                        print("Nome inválido. Tente novamente.")
-                        print()
-                
-    def finalizar(self):
-        ListaCelebridades = []
-        ListaCelebridades.append(self.celebridade)
-        i = 0
-        while i != 3:
-            ListaCelebridades.append(random.choice(self.celebridades))
-            i += 1
-        random.shuffle(ListaCelebridades)
-        maiorVar = max(self.jogadores.values())
-        maiorVoto = []
-        for chave, valor in self.jogadores.items():
-            if valor == maiorVar:
-                maiorVoto.append(chave)
-        if len(maiorVoto) > 1 : 
-            print()
-            print("Fim".center(100, '-').upper())
-            print()
-            print(f'As pessoas com mais votos foram: {", ".join(maiorVoto)}'.center(100, '-').upper())
-
-        else:
-            print()
-            print("Fim".center(100, '-').upper())
-            print()
-            print(f'A pessoa com mais votos foi: {", ".join(maiorVoto)}'.center(100, '-').upper())
-
-        print()
-        print(f"O Impostor era {self.impostor}".center(100, '-').upper())
-        print()
-        print("Qual celebridade era? ".center(100, '-').upper)
-        print()
-        print(f'Pode ser: {", ".join(ListaCelebridades)}'.upper)
-
-        resposta = input("Qual é: ").upper
-        if resposta == self.celebridade.upper():
-            print()
-            print("acertou".upper())
-        else :
-            print()
-            print("não era essa a celebridade".upper())
-
-class JogoVideogames():
-    def __init__(self, nomes):
-        self.jogadores = {}
-        self.j = 1
-        self.Videogames = Videogames
-        self.nomes = nomes
-        self.Videogame = random.choice(self.Videogames)
-        self.perguntas = perguntasVideogames
-        self.impostor = random.choice(self.nomes)
-
-    def part(self):
-        for self.nome in self.nomes:
-            self.jogadores[self.nome] = 0
-
-    def DividirImpostor(self):
-        for pessoa in self.nomes:
-            if pessoa == self.impostor:
-                print()
-                print("Escreva SIM para confirmar")
-                pesCerta = str(input(f"Você é {pessoa}? "))
-                print()
-                if pesCerta == "SIM" or "Sim" or "sim":
-                    print(f" Você é o impostor".center(100, '-').upper())
-                    input("Aperte Qualquer tecla para continuar ")
-                    os.system('cls')
-            else :
-                print()
-                print("Escreva SIM para confirmar")
-                pesCerta = str(input(f"Você é {pessoa}? "))
-                print()
-                if pesCerta == "SIM" or "Sim" or "sim":
-                    print(f" O jogo é: {self.Videogame} ".center(100, '-').upper())
-                    input("Aperte Qualquer tecla para continuar ")
-                    os.system('cls')
-
-    def perguntar(self):
-        for self.nome in self.nomes:
-            print()
-            print(f"{self.nome} Pergunta para: {self.nomes[self.j]}".center(100, '-').upper())
-            print()
-            print(random.choice(self.perguntas))
-            print()
-            input("Aperte Qualquer tecla para continuar ")
-            os.system('cls')
-            self.j += 1
-            if self.j == len(self.nomes):
-                self.j = 0
-    
-    def votar(self):
-     for pessoa in self.nomes:
-            os.system('cls')
-            print("Votação".center(100, '-').upper())
-            print()
-            print("Escreva SIM para confirmar")
-            pesCerta = str(input(f"Você é {pessoa}? "))
-            print()
-            if pesCerta == "SIM" or "Sim" or "sim":
-               posibilitVoto = []
-               for i in self.nomes:
-                   if i != pessoa:
-                    posibilitVoto.append(i)
-            while True:
-                    print(f"Você pode votar em: {', '.join(posibilitVoto)}")
-                    voto = input(f"Votar em: ").upper()
-                    if voto == pessoa:
-                        print()
-                        print("Você não pode votar em si mesmo")
-                        print()
-                    elif voto in posibilitVoto:
-                        self.jogadores[voto] += 1
-                        break  
-                    else:
-                        print()
-                        print("Nome inválido. Tente novamente.")
-                        print()
-                
-    def finalizar(self):
-        ListaVideogames = []
-        ListaVideogames.append(self.Videogame)
-        i = 0
-        while i != 3:
-            ListaVideogames.append(random.choice(self.Videogames))
-            i += 1
-        random.shuffle(ListaVideogames)
-        maiorVar = max(self.jogadores.values())
-        maiorVoto = []
-        for chave, valor in self.jogadores.items():
-            if valor == maiorVar:
-                maiorVoto.append(chave)
-        if len(maiorVoto) > 1 : 
-            print()
-            print("Fim".center(100, '-').upper())
-            print()
-            print(f'As pessoas com mais votos foram: {", ".join(maiorVoto)}'.center(100, '-').upper())
-
-        else:
-            print()
-            print("Fim".center(100, '-').upper())
-            print()
-            print(f'A pessoa com mais votos foi: {", ".join(maiorVoto)}'.center(100, '-').upper())
-    
-        print()
-        print(f"O Impostor era {self.impostor}".center(100, '-').upper())
-        print()
-        print(f"{self.impostor} qual jogo era? ".center(100, '-').upper())
-        print()
-        print(f'Pode ser: {", ".join(ListaVideogames)}'.upper())
-        print()
-        resposta = input("Qual é: ").upper()
-        if resposta == self.Videogame.upper():
-            print()
-            print("acertou".upper())
-        else :
-            print()
-            print("não era esse o jogo".upper())
+            print("não era essa a palavra".upper())
 
 def main():
     nomes = []
@@ -385,13 +153,13 @@ def main():
                 print("Só pode jogar com no minimo 3 pessoas") 
             else :
                 if opcaoJogo == 1:
-                    jogo = JogoComidas(nomes)
+                    jogo = Jogo(nomes=nomes, categoria=comidas, perguntas=perguntasComidas)
                     os.system('cls')
-                if opcaoJogo == 2:
-                    jogo = JogoCelebridades(nomes)
+                elif opcaoJogo == 2:
+                    jogo = Jogo(nomes=nomes, categoria=celebridades, perguntas=perguntasCelebridades)
                     os.system('cls')
-                if opcaoJogo == 3:
-                    jogo = JogoVideogames(nomes)
+                elif opcaoJogo == 3:
+                    jogo = Jogo(nomes=nomes, categoria=Videogames, perguntas=perguntasVideogames)
                     os.system('cls')
    
     jogo.part()
